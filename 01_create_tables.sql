3b8d3d13b6ba520626dda31819c15fe9607be4b7
jenijuli90 <100720472+jenijuli90@users.noreply.github.com>
2025-07-22 13:35:57 -0400

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -11,31 +11,59 @@ IF OBJECT_ID('Courses', 'U') IS NOT NULL DROP TABLE Courses
 IF OBJECT_ID('Students', 'U') IS NOT NULL DROP TABLE Students
 
 -- Students table
-CREATE TABLE Students (
-    StudentID INT PRIMARY KEY IDENTITY(1,1),
-    Name VARCHAR(100) NOT NULL,
-    DateOfBirth DATE
+CREATE TABLE [dbo].[Students] (
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [FirstName] VARCHAR(100) NOT NULL,
+    [LastName] VARCHAR(100) NOT NULL,
+    [Email] VARCHAR(100) NOT NULL,
+    [DateOfBirth] DATE NOT NULL,
+    [IsActive] BIT NOT NULL,
+    [Address] VARCHAR(255) NOT NULL,
+    [PhoneNumber] VARCHAR(20) NOT NULL,
+    [CreatedOn] DATETIME NOT NULL,
+    [UpdatedOn] DATETIME NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
 
+-- Instructors table
+CREATE TABLE [dbo].[Instructors] (
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [InstructorName] VARCHAR(100) NULL,
+    [Department] VARCHAR(100) NOT NULL,
+    [Email] VARCHAR(100) NOT NULL,
+    [IsActive] BIT NOT NULL,
+    [CreatedOn] DATETIME NOT NULL,
+    [UpdateOn] DATETIME NOT NULL,
+    CONSTRAINT [PK_Instructors] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+    
 -- Courses table
-CREATE TABLE Courses (
-    CourseID INT PRIMARY KEY IDENTITY(1,1),
-    CourseName VARCHAR(100) NOT NULL,
-    Credits INT NOT NULL
+CREATE TABLE [dbo].[Courses] (
+    [ID] INT IDENTITY(1,1) NOT NULL,
+    [CourseName] VARCHAR(100) NOT NULL,
+    [Credits] INT NOT NULL,
+    [InstructorId] INT NOT NULL,
+    [IsActive] BIT NOT NULL,
+    [CreatedOn] DATETIME NOT NULL,
+    [UpdatedOn] DATETIME NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED ([ID] ASC)
 )
 
 -- Enrollments table
-CREATE TABLE Enrollments (
-    EnrollmentID INT PRIMARY KEY IDENTITY(1,1),
-    StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
-    CourseID INT FOREIGN KEY REFERENCES Courses(CourseID),
-    EnrollmentDate DATE DEFAULT GETDATE(),
-    Semester VARCHAR(10)
+CREATE TABLE [dbo].[Enrollments] (
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [StudentID] INT NOT NULL,
+    [CourseID] INT NOT NULL,
+    [EnrollmentDate] DATETIME NOT NULL,
+    [Semester] VARCHAR(20) NOT NULL,
+    CONSTRAINT [PK_Enrollments] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
 
 -- Grades table
-CREATE TABLE Grades (
-    GradeID INT PRIMARY KEY IDENTITY(1,1),
-    EnrollmentID INT FOREIGN KEY REFERENCES Enrollments(EnrollmentID),
-    Grade DECIMAL(3,2) CHECK (Grade >= 0 AND Grade <= 4.0)
+CREATE TABLE [dbo].[Grades] (
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [EnrollmentID] INT NOT NULL,
+    [Grade] DECIMAL(3,1) NOT NULL CHECK (Grade >= 0 AND Grade <= 4.0),
+    [GradeDate] DATE NULL,
+    CONSTRAINT [PK_Grades] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
